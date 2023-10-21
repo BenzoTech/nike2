662cc8231986f665e9b33bc4e947c029d82339cf
Benzo Tech <ardiles.md@gmail.com>
2023-10-21 08:15:21 -0300

    second commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomersReviews';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './subscribe';
+import SuperQuality from './SuperQuality';
+
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
